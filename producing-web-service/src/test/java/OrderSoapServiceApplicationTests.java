25b7a42d188085722d783b52af596eeafc2bed8c
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-04-15 17:53:19 +0300

    Create OrderSoapServiceApplicationTests.java

---
+++ producing-web-service/src/test/java/OrderSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.eif.viko.m.nar.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
