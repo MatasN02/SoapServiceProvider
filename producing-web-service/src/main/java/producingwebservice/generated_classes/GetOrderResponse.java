4c4526c34ac9952276b80ace56f3e1e3eba47f66
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-04-15 17:44:50 +0300

    Create GetOrderResponse.java

---
+++ producing-web-service/src/main/java/producingwebservice/generated_classes/GetOrderResponse.java
@@ -0,0 +1,63 @@
+package lt.eif.viko.m.nar.producingwebservice.generated_classes;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="order" type="{http://www.springframework.org/schema/web-services}order"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "order"
+})
+@XmlRootElement(name = "getOrderResponse")
+public class GetOrderResponse {
+
+    @XmlElement(required = true)
+    protected Order order;
+
+    /**
+     * Gets the value of the order property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Order }
+     *     
+     */
+    public Order getOrder() {
+        return order;
+    }
+
+    /**
+     * Sets the value of the order property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Order }
+     *     
+     */
+    public void setOrder(Order value) {
+        this.order = value;
+    }
+
+}
