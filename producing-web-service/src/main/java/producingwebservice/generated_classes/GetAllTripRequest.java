a5c6dddffb62d340fe181a27faed60ba07bb5e41
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-04-15 17:42:58 +0300

    Create GetAllTripRequest.java

---
+++ producing-web-service/src/main/java/producingwebservice/generated_classes/GetAllTripRequest.java
@@ -0,0 +1,14 @@
+package lt.eif.viko.m.nar.producingwebservice.generated_classes;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name="")
+@XmlRootElement(name = "getAllTripRequest")
+public class GetAllTripRequest {
+
+
+}
